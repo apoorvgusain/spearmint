d7f91566401c54fc0cbfa99d5dd4390026dea85a
APOORV GUSAIN <32385555+apoorvgusain@users.noreply.github.com>
2024-06-07 16:53:50 +0530

    Update script.js for logo turn

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
   var logo = document.getElementById("logo");
 
   document.addEventListener("click", function(e) {
+    console.log(e.clientX);
+    console.log(e.clientY);
+    console.log(logo.offsetWidth);
+    console.log(logo.offsetHeight);
+    
+    if (e.clientX > logo.offsetLeft + logo.offsetWidth / 2) {
+          logo.style.transform = "scaleX(-1)";
+        } else {
+          logo.style.transform = "scaleX(1)";
+        }
+    
     var newX = e.clientX - logo.offsetWidth / 2;
     var newY = e.clientY - logo.offsetHeight / 2;
 
